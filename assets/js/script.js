96a015d5e3cd4fb4d5f14c56a6d9330506aeb4d0
“Ivan” <vlasoff.ivan95@gmail.com>
2022-10-04 19:32:38 +0300

    2 task

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,5 +7,26 @@ function calcElements (n) {
         sumElements +=i;
     };
     return sumElements;
-}
-console.log(5)
+};
+console.log(calcElements(5))
+
+// //2.1 Создать объект student который содержит следующие свойства: имя, фамилию, пол, контактные данные, id.
+// 2.2 Создать объект студентской группы, содержащий имя университета, факультета и кафедры 
+// 2.3 Связать обьект студента с обьектом его группы
+// 2.4 Реализовать функцию вывода на экран всей информации о студенте (включая и информацию, связанную с универом) в произвольном виде. 
+//Функция должна принимать обьект студента
+
+const Student = {
+    firstName:'Oleksii',
+    lastName : 'Arestovich',
+    gender: 'Male',
+    contact:'099-333-22-11',
+    id:1,
+};
+
+const University = {
+    nameUniversity:"ZNTU",
+    faculty:"engineering physics",
+    department:"applied mechanics",
+};
+
